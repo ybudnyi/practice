9b84b9eb52108ef8feab3054fb7087bc24a95f0f
ybudnyi <ybudnyi@gmail.com>
2022-01-11 10:40:45 +0200

    add tf db module

--- Terraform/modules/db/db.tf
+++ Terraform/modules/db/db.tf
@@ -1,6 +1,7 @@
 resource "aws_db_instance" "dashvis_db" {
   db_subnet_group_name = var.db_subnet_group_name
-  allocated_storage    = 5
+  allocated_storage    = 50
+  max_allocated_storage = 100
   engine               = var.engine
   engine_version       = var.engine_version
   instance_class       = var.instance_class
@@ -8,6 +9,10 @@ resource "aws_db_instance" "dashvis_db" {
   username             = var.username
   password             = var.password
   skip_final_snapshot  = true
+  copy_tags_to_snapshot = true
+  delete_automated_backups = false
+  deletion_protection = true
+  publicly_accessible = false
   vpc_security_group_ids = [var.sec_group]
   tags = {
     ita_group = "Lv-6130"
